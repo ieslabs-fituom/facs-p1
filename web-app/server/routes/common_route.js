0916b082e453045f5e2e20677fda6a5e7e83d79f
Lakmina Pramodya Gamage <pramodyalakmina@gmail.com>
2023-07-10 00:03:24 +0530

    stable release

--- web-app/server/routes/common_route.js
+++ web-app/server/routes/common_route.js
@@ -6,5 +6,6 @@ const common_controller = require('../controllers/common_controller.js');
 router.get('/',common_controller.sign_in_view);
 router.get('/signin',common_controller.sign_in_view);
 router.post('/signin/contdsign',common_controller.continue_sign_in);
+router.get('/signout',common_controller.continue_sign_out);
 
 module.exports = router;
