9b00c2fd830b9fc5e0dea5b5f21dabd4ea543de6
Sankha Bimsara <sankha.b21@gmail.com>
2023-08-30 13:16:20 +0530

    API for device added

--- web-app/server/controllers/common_controller.js
+++ web-app/server/controllers/common_controller.js
@@ -19,7 +19,6 @@ exports.continue_sign_in = async (req, res) => {
     let email = req.body.email;
     let password = req.body.password;
 
-
     let loadUserDetails = (email) => {
         return new Promise((resolve, reject) => {
             let sql = 'SELECT id,Designation,Password FROM employees WHERE Email = "' + email + '"';
@@ -60,4 +59,99 @@ exports.continue_sign_out = async (req, res) => {
     process.env.CURRENT_ID = "";
     process.env.CURRENT_TYPE = "";
     res.render('sign-in');
+}
+
+exports.postattendance = async (req, res) => {
+    let api_key = 'tPmAT5Ab3j7F9';
+    console.log("Started");
+    if (req.body.api_key != api_key) {
+        console.log("Wrong API key");
+        res.send(212);
+        return;
+    }
+
+    let session_id = req.body.session_id;
+    let attendance = req.body.attendance;
+    console.log(session_id, attendance);
+
+
+    // Select the student group of the session
+    let getStudentGroup = async (session_id) => {
+        return new Promise((resolve, reject) => {
+            let sql = 'SELECT Ses_group FROM sessions WHERE id = ' + session_id;
+            conn.query(sql, (err, rows) => {
+                if (!err) {
+                    return resolve(rows);
+                } else {
+                    return reject(err);
+                }
+            })
+        })
+    }
+
+    console.log("Loading student group");
+
+    let studentGroup;
+    try{
+        studentGroup = await getStudentGroup(session_id);
+    } catch(err) {
+        console.log(err);
+        res.send({ status: 500 });
+        return;
+    }
+
+    if (studentGroup.length == 0 || studentGroup == null) {
+        console.log("No student group found");
+        res.send({ staus: 201 });
+        return;
+    }
+
+    studentGroup = studentGroup[0].Ses_group;
+    let updatingSQL = 'UPDATE attendance_' + studentGroup + ' SET ses' + session_id + ' = CASE ';
+
+    let student_uid, attendance_time;
+    let studentIDs = '';
+    for (let studentAttendance of attendance) {
+        student_uid = studentAttendance.substring(0, studentAttendance.length - 9);
+        attendance_time = studentAttendance.substring(studentAttendance.length - 8);
+
+        updatingSQL += 'WHEN Student = "' + student_uid + '" THEN "' + attendance_time + '" ';
+        studentIDs += '"' + student_uid + '",';
+    }
+
+    updatingSQL += 'END WHERE Student IN (' + studentIDs.substring(0, studentIDs.length - 1) + ')';
+    console.log(updatingSQL);
+
+    let updateAttendance = async (updatingSQL) => {
+        return new Promise((resolve, reject) => {
+            conn.query(updatingSQL, (err, rows) => {
+                if (!err) {
+                    return resolve(rows);
+                } else {
+                    return reject(err);
+                }
+            })
+        })
+    }
+
+    console.log("Updating attendance");
+    let result;
+    try{
+        result = await updateAttendance(updatingSQL);
+    } catch(err) {
+        console.log(err);
+        res.send({ status: 500 });
+        return;
+    }
+
+    if(!result){
+        console.log("Error updating attendance");
+        res.send({ status: 201 });
+        return;
+    }else{
+        console.log("Attendance updated");
+        res.send({ status: 200 });
+        return;
+    }
+
 }
--- web-app/server/routes/dist/common_route.dev.js
+++ web-app/server/routes/dist/common_route.dev.js
@@ -8,6 +8,8 @@ var common_controller = require('../controllers/common_controller.js'); // Route
 
 
 router.get('/', common_controller.sign_in_view);
+router.post('/postattendance', common_controller.postattendance);
 router.get('/signin', common_controller.sign_in_view);
 router.post('/signin/contdsign', common_controller.continue_sign_in);
+router.get('/signout', common_controller.continue_sign_out);
 module.exports = router;
