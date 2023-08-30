f7ea6848873d3a834d0c5079d90e43e8062e162d
Sankha Bimsara <sankha.b21@gmail.com>
2023-08-30 13:16:20 +0530

    API for device added

--- web-app/server/routes/common_route.js
+++ web-app/server/routes/common_route.js
@@ -4,6 +4,7 @@ const common_controller = require('../controllers/common_controller.js');
 
 // Routes for sign in
 router.get('/',common_controller.sign_in_view);
+router.post('/postattendance',common_controller.postattendance);
 router.get('/signin',common_controller.sign_in_view);
 router.post('/signin/contdsign',common_controller.continue_sign_in);
 router.get('/signout',common_controller.continue_sign_out);
